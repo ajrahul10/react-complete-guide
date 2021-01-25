e800d14e1ad239bd5a04c8c37892c267c3883808
Rahul Ajmani <rahulajmani910@gmail.com>
2021-01-25 08:51:15 +0530

    adding styling

--- src/App.js
+++ src/App.js
@@ -27,10 +27,19 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hey man wassup!</h1>
-        <button onClick={() => this.switchNameHandler("Aditya")}>Switch name</button>
+        <button style={style} onClick={() => this.switchNameHandler("Aditya")}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person changed={this.changeNameHandler} click={this.switchNameHandler.bind(this, "Adi")} name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies include: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
