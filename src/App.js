0326fdd91adb0f789cd6793086a9ae23a13489df
Rahul Ajmani <rahulajmani910@gmail.com>
2021-03-13 23:52:52 +0530

    adding styles and files

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import React, {Component} from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, {Component} from 'react'
+import Person from './Person/Person'
+import './App.css'
 
 class App extends Component {
 
   state = {
-    persons: [
+    person: [
       {name: 'Rahul', age: 27},
-      {name: 'Sameer', age: 24},
-      {name: 'Akshay', age: 26}
+      {name: 'Aditya', age: 26},
+      {name: 'Sameer', age: 24}
     ]
   }
 
-  switchNameHandler = (newName) => {
-    //console.log('switch clicked');
-    let s = {...this.state};
-    s.persons[2].name = newName;
-    this.setState(s);
+  buttonClickHandler = (newName) => {
+    this.setState({
+      person: [
+        {name: 'Rahul', age: 27},
+        {name: newName, age: 26},
+        {name: 'Sameer', age: 24}
+      ]
+    })
   }
 
-  changeNameHandler = (event) => {
-    //console.log('switch clicked');
-    let s = {...this.state};
-    s.persons[1].name = event.target.value;
-    this.setState(s);
+  textChangeHandler = (event) => {
+    let name = event.target.value;
+    this.setState({
+      person: [
+        {name: 'Rahul', age: 27},
+        {name: name, age: 26},
+        {name: 'Sameer', age: 24}
+      ]
+    })
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     return (
       <div className="App">
-        <h1>Hey man wassup!</h1>
-        <button style={style} onClick={() => this.switchNameHandler("Aditya")}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person changed={this.changeNameHandler} click={this.switchNameHandler.bind(this, "Adi")} name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies include: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <h1>Hello!</h1>
+        <button onClick={this.buttonClickHandler.bind(this, 'Akshay')}>Click me</button>
+        <Person 
+          name={this.state.person[0].name} 
+          age={this.state.person[0].age} />
+        <Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age}
+          changeName={() => this.buttonClickHandler('Akku')}
+          textChange={this.textChangeHandler}>My hobbies: Luxury</Person>
+        <Person 
+          name={this.state.person[2].name} 
+          age={this.state.person[2].age} />
       </div>
-    )
+    );
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import './Person.css';
+import './Person.css'
 
-const person = (props) => {
-
+const Person = (props) => {
     return (
         <div className="person">
-            <p onClick={props.click}>I'm a {props.name}. I am {props.age} years old</p>
+            <div onClick={props.changeName}>My name is {props.name} and I'm {props.age} years old</div>
             <p>{props.children}</p>
-            <input name="text" onChange={props.changed} value={props.name} />
+            <input type="text" onChange={props.textChange} value={props.name}/>
         </div>
     )
 }
 
-export default person;
+export default Person;
