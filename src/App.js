f6c2d81527207f2a95af9adf884ed2dc16efdc64
Rahul Ajmani <rahulajmani910@gmail.com>
2021-06-13 22:16:30 +0530

    modifying project to building expense tracker

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Expenses from './components/Expenses'
+
+function App() {
+	const expenses = [
+		{
+			id: "e1",
+			title: "Toilet Paper",
+			amount: 26.78,
+			date: new Date(2021, 5, 5),
+		},
+		{
+			id: "e2",
+			title: "New TV",
+			amount: 874.58,
+			date: new Date(2021, 4, 28),
+		},
+		{
+			id: "e3",
+			title: "Car Insurance",
+			amount: 360.23,
+			date: new Date(2021, 6, 1),
+		},
+		{
+			id: "e4",
+			title: "New Desk",
+			amount: 409.49,
+			date: new Date(2021, 1, 12),
+		},
+	];
+
+	return (
+		<div>
+			<h1>This is my first app!</h1>
+			<Expenses expenses={expenses} />
+		</div>
+	);
+}
+
+export default App;
