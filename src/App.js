3d5b3f071253cd8ecb7463d2aa14801d48045d54
Rahul Ajmani <rahulajmani910@gmail.com>
2021-06-16 23:03:20 +0530

    coded year filter logic

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
 
+
+const DUMMY_EXPENSES = [
+	{
+		id: "e1",
+		title: "Toilet Paper",
+		amount: 26.78,
+		date: new Date(2021, 5, 5),
+	},
+	{
+		id: "e2",
+		title: "New TV",
+		amount: 874.58,
+		date: new Date(2021, 4, 28),
+	},
+	{
+		id: "e3",
+		title: "Car Insurance",
+		amount: 360.23,
+		date: new Date(2021, 6, 1),
+	},
+	{
+		id: "e4",
+		title: "New Desk",
+		amount: 409.49,
+		date: new Date(2021, 1, 12),
+	},
+];
+
 const App = () => {
-	const expenses = [
-		{
-			id: "e1",
-			title: "Toilet Paper",
-			amount: 26.78,
-			date: new Date(2021, 5, 5),
-		},
-		{
-			id: "e2",
-			title: "New TV",
-			amount: 874.58,
-			date: new Date(2021, 4, 28),
-		},
-		{
-			id: "e3",
-			title: "Car Insurance",
-			amount: 360.23,
-			date: new Date(2021, 6, 1),
-		},
-		{
-			id: "e4",
-			title: "New Desk",
-			amount: 409.49,
-			date: new Date(2021, 1, 12),
-		},
-	];
+
+	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 	const addExpenseHandler = expense => {
-		console.log('In app.js');
-		expenses.push(expense);
-		console.log(expenses);
-
+		setExpenses(prevState => {
+			return [expense, ...prevState]
+		});
 	}
 
 	return (
